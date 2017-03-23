4d66323171120fe4f540ab60575cd8dc32c7f627
bruno.sandre <bruno.sandre@CAS555576W0723.senacsp.edu.br>
2017-03-23 20:22:36 -0300

    Solução de Baskara utilizando o next.Double

---
+++ src/baskara/Baskara.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baskara;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruno.sandre
+ */
+public class Baskara {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+      
+        //ENTRADA
+        System.out.print("x1: ");
+        double x1 = console.nextDouble();
+        
+        System.out.print("x2: ");
+        double x2 = console.nextDouble();
+        
+        System.out.print("C: ");
+        double C = console.nextDouble();
+        
+        //Processamento
+        double s  = x1 + x2;
+        double p = x1 * x2;
+        double a = C / p;
+        double b = -(s * a);
+        
+        // Saida
+         System.out.println(" A equação que tem raizes "
+                  + x1 
+                  + "e "
+                  + x2 
+                  + " é "
+                  + a + "x² + "
+                         
+                  + b+ "x "
+                       + C);
+             
+        
+    }
+
+}
